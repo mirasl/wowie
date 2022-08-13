f0edb607454147f6d554b24b3e88d8464b5aa011
mirasl <santaluciamira+chia@gmail.com>
2022-08-12 21:43:16 -0700

    Finished intro and added fadeout

---
+++ SCENES/Gate.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+const SPINOUT_DURATION = 0.8
+
+func _on_Area2D_area_entered(area):
+	if area.get_collision_layer_bit(4): # sword
+		$CollisionShape2D.disabled = true
+		var t1 = get_tree().create_tween()
+		var t2 = get_tree().create_tween()
+		t1.tween_property(self, "position", Vector2(-600, 600), SPINOUT_DURATION)
+		t2.tween_property(self, "rotation_degrees", 720.0, SPINOUT_DURATION)
+		yield(get_tree().create_timer(SPINOUT_DURATION), "timeout")
+		queue_free()
--- SCENES/Office.gd
+++ SCENES/Office.gd
@@ -1,8 +1,9 @@
 extends "res://SCENES/Screenplay.gd"
 
 var textbox
-var check1 = true
-var check2 = true
+var check1 := true
+var check2 := true
+var check3 := true
 
 # script for intro
 func _ready():
@@ -14,6 +15,8 @@ func _ready():
 	
 	yield(get_tree().create_timer(1), "timeout")
 	textbox = load_text_custom_speed("intro.json", 0.03)
+	
+	$Overlay.hide()
 
 
 func _physics_process(delta):
@@ -35,6 +38,12 @@ func _physics_process(delta):
 				$AnimationPlayer.play("walk_to_door")
 				check2 = false
 				textbox.queue_free()
+		
+		if textbox.dialogPath == "res://dialogue/intro3.json":
+			if textbox.phraseNum == -1 and check3:
+				$AnimationPlayer.play("leave_room")
+				check3 = false
+				textbox.queue_free()
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
@@ -46,6 +55,14 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		yield(get_tree().create_timer(0.2), "timeout")
 		$ChargeBot.launch()
 		$AnimationPlayer.play("dodge_robot")
+		yield(get_tree().create_timer(2), "timeout")
+		textbox = load_text("intro3.json")
+	
+	elif anim_name == "leave_room":
+		$Overlay.show()
+		$Overlay.modulate.a = 0
+		var t = get_tree().create_tween()
+		t.tween_property($Overlay, "modulate", Color(0, 0, 0, 1), 1)
 
 
 func _on_ChargeBot_get_player_position():
