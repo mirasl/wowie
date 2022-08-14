fe826a4a5626b617a012a22285aeb1e1af54484e
mirasl <santaluciamira+chia@gmail.com>
2022-08-14 14:56:11 -0700

    Added titlescreen song

--- SCENES/StartScreen.gd
+++ SCENES/StartScreen.gd
@@ -5,8 +5,10 @@ extends Node2D
 func _ready():
 	$Camera2D/AnimationPlayer.playback_speed = 0.3
 	$Camera2D/AnimationPlayer.play("pan")
+	Audio.play("TitleScreen")
 
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("accept"):
+		Audio.stop("TitleScreen")
 		get_tree().change_scene("res://SCENES/Office.tscn")
